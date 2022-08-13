27ceb76f9de3c8bf43752d64c03cbeafaad38e29
TeoNemsadze <87779348+TeoNemsadze@users.noreply.github.com>
2022-08-13 16:02:30 +0400

    Add files via upload

---
+++ travel_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  travel_appApp.swift
+//  travel-app
+//
+//  Created by teona nemsadze on 13.08.22.
+//
+
+import SwiftUI
+
+@main
+struct travel_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
